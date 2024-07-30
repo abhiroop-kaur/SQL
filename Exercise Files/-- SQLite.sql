4c6584597f61954d0494539571e10218ca1ec37c
Abhiroop Kaur <abhiroopk.o@gmail.com>
2024-07-29 21:29:40 -0600

    new info

--- Exercise Files/-- SQLite.sql
+++ Exercise Files/-- SQLite.sql
@@ -1,19 +1,32 @@
 -- SQLite
---query pane
-/*
-created by abhiroop kaur
-date july 2, 2027
-just working around some databases
-*/
-
---list of customers in databases first name , lastname 
+--% means idc what comes next
 SELECT
-    FirstName AS [Customer FirstName],
-    LastName AS [Customer LastName], 
-    Email AS EMAIL
-FROM 
-    Customer
+    InvoiceDate,
+    BillingAddress,
+    BillingCity,
+    Total,
+    CASE
+        WHEN Total<2.00 THEN 'Baseline Purchase'
+        WHEN Total BETWEEN 2.00 AND 6.99 THEN 'Low Purchase'
+        WHEN Total BETWEEN 7.00 AND 15.00 THEN 'Target Purchase'
+        ELSE 'Top Performer'
+        END AS PurchaseType
+FROM
+    Invoice
+WHERE
+    --Total IN (1.98, 3.96) --or
+    --BETWEEN 1.98 AND 5.00
+    --BillingCity IN ('Brussels','Orlando', 'Paris')
+    --BillingCity LIKE 'B%' cities starting with B
+    --BillingCity LIKE '%B%'  b anywhere in name
+    --InvoiceDate = '2010-05-22 00:00:00'
+    --(BillingCity  LIKE 'P%'OR BillingCity  LIKE 'D%')
+    --AND Total> 1.98
+
+    --DATE(InvoiceDate )> '2010-05-22'
+    --AND
+    --Total<3.00
+    PurchaseType = 'Top Performer'
+
 ORDER BY
-    LastName ASC,
-    FirstName DESC
-LIMIT 10
+    BillingCity
