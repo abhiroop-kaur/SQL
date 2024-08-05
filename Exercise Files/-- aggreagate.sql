c3849292955553040d43198ce39c96ad67e34eab
Abhiroop Kaur <abhiroopk.o@gmail.com>
2024-08-05 11:03:27 +0530

    aggreagte and functions

---
+++ Exercise Files/-- aggreagate.sql
@@ -0,0 +1,19 @@
+-- aggreagate
+SELECT
+    SUM(Total) AS [Total Sales],
+    round(AVG(Total),2),
+    AVG(Total) AS [Average Sales],
+    MAX(Total) AS [maximum],
+    MIN(Total) AS [minimum],
+    COUNT(*) AS [Sales count]
+FROM
+Invoice
+
+
+
+--avg total for each city
+SELECT
+Bill
+
+FROM
+Invoice
